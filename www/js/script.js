710ca1e5385909511dc13ce98c76aa93453c2ab3
Matthew Leeds <mwl458@gmail.com>
2014-08-01 03:38:11 -0400

    Changed css a bit and added user/pass fields.

--- www/js/script.js
+++ www/js/script.js
@@ -1,8 +1,7 @@
 /* 
  * File: script.js
  * Author: Matthew Leeds
- * Last Edit: 7.28.2014
- *
+ * Last Edit: 7.31.2014
  */
 
 var courseData;
@@ -16,6 +15,8 @@ function main() {
             addSubjects();
         }
     });
+    $('#inputform').submit(function(event) {//TODO});
+
 }
 
 // populate a dropdown with Subjects 
@@ -29,9 +30,10 @@ function addSubjects() {
                                         .attr("onchange", "addCourses()");
     for (var i = 0; i < subjects.length; i++) {
         $select.append($("<option></option>").attr("value", subjects[i])
-                                             .text(subjects[i]));
+                                             .text(subjects[i]))
+               .prop("selectedIndex", -1);
     }
-    $("tbody tr:nth-child(2)").empty().append($("<td></td>").text("Select a subject: ")
+    $("tbody tr:nth-child(4)").empty().append($("<td></td>").text("Select a subject: ")
                                                             .css("text-align", "right"))
                                       .append($("<td></td>").append($select)
                                                             .css("text-align", "left"));
@@ -40,26 +42,43 @@ function addSubjects() {
 // populate a dropdown with courses based on the selected subject
 function addCourses() {
     subject = $("#subjectsSelect").val();
+    var courses = []
+    for (var key in courseData[subject]) {
+        courses.push(key)
+    }
+    courses.sort();
     var $select = $("<select></select>").attr("id", "coursesSelect")
                                         .attr("onchange", "addSections()");
-    var courses = courseData[subject];
     for (var i = 0; i < courses.length; i++) {
         $select.append($("<option></option>").attr("value", courses[i])
-                                             .text(courses[i]));
+                                             .text(courses[i]))
+               .prop("selectedIndex", -1);
     }
-    $("tr:nth-child(3)").empty().append($("</td><td>").text("Select a course: ")
+    $("tr:nth-child(5)").empty().append($("<td></td>").text("Select a course: ")
                                                       .css("text-align", "right"))
                                 .append($("<td></td>").append($select)
                                                       .css("text-align", "left"));
 }
 
-/*function addSections() {
-    $("#sectionsSelect").remove();
+function addSections() {
     course = $("#coursesSelect").val();
-    var select = $("<select/>");
-    select.attr("id", "sectionsSelect");
+    var sections = courseData[subject][course];
+    var $select = $("<select></select>").attr("id", "sectionsSelect")
+                                        .attr("onchange", "enableSubmit()");
+    for (var i = 0; i < sections.length; i++) {
+        $select.append($("<option></option>").attr("value", sections[i])
+                                             .text(sections[i]))
+               .prop("selectedIndex", -1);
+    }
+    $("tr:nth-child(6)").empty().append($("<td></td>").text("Select a section: ")
+                                                      .css("text-align", "right"))
+                                .append($("<td></td>").append($select)
+                                                      .css("text-align", "left")); 
+}
 
-}*/
+function enableSubmit() {
+    $('#checkButton').prop('disabled', false);
+}
 
 $(document).ready(function() {
     main();
